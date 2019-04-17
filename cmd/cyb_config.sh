56456bfb0935574aa348f2661681efaeda35453e
sunqi <sun.qi@cybex.io>
2019-04-17 19:21:20 +0800

    cybex config.ini bug fix

--- cmd/cyb_config.sh
+++ cmd/cyb_config.sh
@@ -2,4 +2,5 @@
 # FROM=/home/sunqi/dex/tnet/
 FROM=${DEPLOY_DIR}
 for i in {1..11}; do cp ${FROM}/cybex/* ${FROM}/node$(($i-1))/data/; done
+for i in {1..11}; do sed -i "s/node_to_be_replace/node$(($i-1))/g" ${FROM}/node$(($i-1))/data/config.ini; done
 
